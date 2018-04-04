0b944656c7f0e804fd001e85938d89d4883f1681
napier40401628 <myonandastarlord@gmail.com>
2018-04-03 22:52:05 -0700

    format prev and next buttons

--- server.py
+++ server.py
@@ -47,11 +47,17 @@ def countryByNamePage(n):
 	return render_template(
 		'country.html',
 		c = c)
-
+@app.route('/delete/<n>')
+def deleteCountry(n):
+        i = 0
+        for c in w:
+                if c['name'] == n:
+                        break
+                i = i+1
+        del w[i]
+        return render_template('index.html',
+		w = w[0:page_size],
+		page_number = 0,
+		page_size = page_size
+		) 
 app.run(host='0.0.0.0', port=5628, debug=True)
-
-
-
-
-
-
